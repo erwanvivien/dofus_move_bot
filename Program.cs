9d4f2308eebb1f4152ac484b54cd4c3bb1259a96
erwan.vivien <erwan.vivien@epita.fr>
2021-05-05 13:10:22 +0200

    :sparkles: Added a class to take screenshots of an application

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace dofus_move_bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ScreenCapture sc = new ScreenCapture();
+
+            Image img = sc.CaptureScreen();
+            
+            // capture this window, and save it
+            sc.CaptureWindowToFile((IntPtr) 0x008306D8, "C:\\Users\\Erwan\\Desktop\\test.jpeg",ImageFormat.Jpeg);
+        }
+    }
+}
