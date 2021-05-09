a0815e1b6a41f8a4da09e05ef772f2932c8d18ff
erwan.vivien <erwan.vivien@epita.fr>
2021-05-09 11:07:29 +0200

    :building_construction: Moved file to src folder

---
+++ src/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Drawing.Imaging;
+using System.Threading;
+
+namespace dofus_move_bot
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var dof = new DofusMap();
+        }
+    }
+}
